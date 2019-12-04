cee35109f9c3a027bef09b9ff197a8b812ae4149
GioLopez <giovanni.lopez@cwc.com>
2019-12-04 15:56:47 -0500

    Moving

--- thesite/polls/views.py
+++ thesite/polls/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Ey soul sister!')
+    return HttpResponse("Ey soul sister!, you are at home")
+
+def detail(request, question_id):
+    return HttpResponse("Your are in details of question: {}".format(question_id))
+
+def results(request, question_id):
+    return HttpResponse("Your are watching results of question: {}".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You are voting question {}".format(question_id))

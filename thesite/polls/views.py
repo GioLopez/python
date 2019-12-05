62c30b826ad835909d862028ebda5d458f104f7b
Giovanni Lopez <giovanni.lopez@cwc.com>
2019-12-04 22:15:07 -0500

    Added more templates, use generic list and details views

--- thesite/polls/views.py
+++ thesite/polls/views.py
@@ -1,26 +1,46 @@
+from django.urls import reverse
+from django.db.models import F
+from django.views  import generic
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from .models import Question, Choice
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    context = {
-            "latest_question_list": latest_question_list,
-            }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question}) 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    return HttpResponse("Your are watching results of question: {}".format(question_id))
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You are voting question {}".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        print(request.POST)
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message':"you didn't select a choice"},
+                )
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse(
+            "polls:results", args=(question.id,))
+            )
 
 def getChoice(request, question_id):
     choice_given_pk = Choice.objects.filter(question_id=question_id)
